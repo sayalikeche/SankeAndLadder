46d270317d632c48275f45e61a13262bfcca4c3d
sayalikeche <kechesayali11@gmail.com>
2021-11-11 05:29:18 +0530

    [sayali welcome to Sanke And Ladder]

---
+++ SankeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SankeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Sanke and ladder");
+            Console.ReadLine();
+        }
+    }
+}
